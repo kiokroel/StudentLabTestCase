6c851536f0bc1335d151d9c786cff9e501f5ea52
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-04-30 21:35:09 +0600

    .

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routers.users_router import router as users_router
 from app.routers.forms_router import router as forms_router
+from auth.auth_router import router as auth_router
 
 app = FastAPI(
     title="Forms App")
 
 app.include_router(users_router)
 app.include_router(forms_router)
+app.include_router(auth_router)
