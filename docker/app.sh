650da7413ff775f1f93c317f1ad5e5be5cc5ab6f
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-05-04 16:24:32 +0600

    unicorn to gunicorn form Docker

--- docker/app.sh
+++ docker/app.sh
@@ -4,4 +4,4 @@ while !</dev/tcp/db/5432;
 do sleep 1;
 done;
 alembic upgrade head;
-uvicorn main:app --host 0.0.0.0 --port 8000
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
